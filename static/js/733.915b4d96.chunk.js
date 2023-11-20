098f663aa03edcf4161bd32359a63ccdc3bbd496
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-11-20 11:38:05 +0000

    Deploying to gh-pages from @ CodeOlga/car-park-test@4f2c796bf9257a1fe4630c038064fb66e6473d56 🚀

--- static/js/733.915b4d96.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[733],{3733:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("h1",{children:"Favorites"})})}}}]);
-//# sourceMappingURL=733.915b4d96.chunk.js.map
